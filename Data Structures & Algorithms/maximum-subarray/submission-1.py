26190b6f1056d6da1b8a08f0c023e9604b145781
Aakriti Jain <163650998+aacritea@users.noreply.github.com>
2026-06-06 23:02:20 +0530

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0] #first subarray will be first element
+        currentsum = 0
+
+        for n in nums:
+            if currentsum < 0:
+                currentsum = 0 #negative values are redundant in sum
+            currentsum += n
+            maxSub = max(currentsum, maxSub)
+        return maxSub
+        
